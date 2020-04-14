ab76f88df18e890b3f74fedef07f037c345b53f1
yudh24 <yudha.mawon24@gmail.com>
2020-04-14 16:11:38 +0700

    14 april 2020 #2

--- netbot1.py
+++ netbot1.py
@@ -13,22 +13,18 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=hostname, username=username, password=password, allow_agent=False, look_for_keys=False)
 
 config_template = input('Nomer Template : ')
-template = 'config_list'+config_template
 
-print(template)
+if config_template == "1":
+    config_list = [
+        'ip dns set servers=5.5.5.5',
+        'ip address add address=192.168.1.1/24 interface=ether2',
+    ]
+else:
+    config_list = [
+        'ip dns set servers=2.2.2.2',
+        'ip address add address=192.168.2.1/24 interface=ether2',
+    ]
 
-list1 = [
-    'ip dns set servers=5.5.5.5',
-    'ip address add address=192.168.1.1/24 interface=ether2',
-]
-
-list2 = [
-    'ip dns set servers=6.6.6.6',
-    'ip address add address=192.168.2.1/24 interface=ether2',
-]
-
-
-
-for config in getattr():
+for config in config_list:
     ssh_client.exec_command(config)
     time.sleep(0.2)
